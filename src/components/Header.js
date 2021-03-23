b00f8557fd229447479e53a7bab660fc89449eac
RadientBrain <maheshwarichetan00@gmail.com>
2021-03-23 11:07:33 +0530

    Header Completed

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Typed from 'react-typed';
+import profilePic from '../profile.png'
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                    <img className="profile-pic" src={profilePic} alt="profile-pic"/>
+                <b>
+                    <h1>
+                        Hi, I am Chetan Maheshwari
+                    </h1>
+
+                    <Typed
+                    
+                    className="typed-text"
+                    strings={[	"We become what we think about...", 
+                    "Winning isn\'t everything,but wanting to win is...", 
+                    "Either you run the day, or the day runs you", 
+                    "Fall seven times and stand up eight",
+                    "Everything has beauty, but not everyone can see"]}
+                    typeSpeed={60}
+                    backSpeed={60}
+                    loop
+                    />
+                </b>
+
+                <a href="#" className="btn-resume-download-link">  Resume </a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
